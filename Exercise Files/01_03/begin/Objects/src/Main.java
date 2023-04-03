a208a3223e8aff3bc99f1f58e77bad1a4df18753
Joey <thejosephacevedo@gmail.com>
2023-04-03 11:27:04 -0400

    announceTallTree method

--- Exercise Files/01_03/begin/Objects/src/Main.java
+++ Exercise Files/01_03/begin/Objects/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        Tree myFavoriteOakTree = new Tree(120,
-                12, TreeType.OAK);
+        Tree myFavoriteOakTree = new Tree(120, 12, TreeType.OAK);
+
+        myFavoriteOakTree.announceTallTree();
+
+        Tree myFavoriteMapleTree = new Tree(90, 30, TreeType.MAPLE);
+
+        myFavoriteMapleTree.announceTallTree();
     }
 }
