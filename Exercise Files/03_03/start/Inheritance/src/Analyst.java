ed4d3feb3722ab6f21657e5c7a9470fdeb2d9b76
Joey <thejosephacevedo@gmail.com>
2023-04-05 12:44:42 -0400

    Inheritance Vid 3

--- Exercise Files/03_03/start/Inheritance/src/Analyst.java
+++ Exercise Files/03_03/start/Inheritance/src/Analyst.java
@@ -1,26 +1,9 @@
-public class Analyst {
-
-    private String name;
-    private double salary;
-    private int age;
+public class Analyst extends Employee {
 
     public Analyst(String name, double salary, int age) {
-        this.name = name;
-        this.salary = salary;
-        this.age = age;
+        super(name, salary, age);
     }
 
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public double getSalary() {
-        return this.salary;
-    }
 
     public double getAnnualBonus() {
         return this.salary * .05;
